93dc3d725979e9a8b64419147d2ac1b88274bb3f
Dave Marley <dmarley@rockets.utoledo.edu>
2020-02-10 17:57:01 -0500

    HW1 - Hello World

---
+++ HW1/src/edu/utoledo/Main.java
@@ -0,0 +1,9 @@
+package edu.utoledo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.console().printf("\nHello World\n\n");
+    }
+}
